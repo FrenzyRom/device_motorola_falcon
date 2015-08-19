62f51eb17f3978a03915c10ac69994bd95cf8cc4
TechVeerA <techveera007@gmail.com>
2015-08-20 02:02:59 +0530

    Add UberTC

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,6 +27,15 @@ TARGET_BOARD_INFO_FILE := device/motorola/falcon/board-info.txt
 # Kernel
 BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/mkbootimg.mk
 TARGET_KERNEL_CONFIG := falcon_defconfig
+ENABLE_GRAPHITE := true
+
+# Kernel Toolchain
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.9/bin
+KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
+
+# Rom Toolchain
+TARGET_GCC_VERSION_EXP := 4.9
+TARGET_KERNEL_SOURCE := kernel/motorola/msm8226
 
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_msm
@@ -41,6 +50,17 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 5930598400 # 5930614784 - 16384
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.qcom
+
+# Compiler flags
+TARGET_GCC_VERSION_EXP := 4.9
+TARGET_USE_PIPE := true
+USE_O3_OPTIMIZATIONS := true
+SUPPRES_UNUSED_WARNING := true
+KRAIT_TUNINGS := true
+ENABLE_GCCONLY := true
+GRAPHITE_OPTS := true
+STRICT_ALIASING := true
+BOOT_ZIP_OUT_FILE := OrionKernel-$(TARGET_DEVICE)-$(PLATFORM_VERSION)-$(shell date +"%Y%m%d")
 
 # inherit from the proprietary version
 -include vendor/motorola/falcon/BoardConfigVendor.mk
